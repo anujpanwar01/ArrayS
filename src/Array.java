46001e35a6914daa42ad846f085f29d6967b6963
Anuj Panwar <anujpanwar7911@gmail.com>
2022-09-27 07:31:21 +0530

    Array

---
+++ src/Array.java
@@ -0,0 +1,26 @@
+public class Array {
+    public static void main(String[] args) {
+        // Array in Java
+        //   1. Way to create Array in Java
+
+        // int represent which type of datatype will store in this array. int[5] 5 represent the length of Array
+        int[] num = new int[5];
+
+        // This is the declaration of array.
+        int[] num2;
+
+        // initialization array in memory heap.
+        num2=new int[5];
+
+
+        // 2 way to create array in java
+        int[] num1 = {};
+
+        // num is reference variable in Stack memory
+        // new int[5] store the object in heap memory
+
+        // int[] num happen at runtime
+        // new int[5] happen at compile time.
+
+    }
+}
